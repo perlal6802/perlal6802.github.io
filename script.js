286162f2024eb35a7856af67a68abb6bb0512f63
perlal6802 <56261627+perlal6802@users.noreply.github.com>
2019-10-07 09:44:17 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Perla Lopez"
+let userName = "Perlal6802";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
